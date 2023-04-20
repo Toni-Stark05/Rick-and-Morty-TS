3e6b1ab6868354e71508ab7badd5fab1dde42ff5
Toni-Stark05 <nikidysbdbdy@gmail.com>
2023-04-20 12:53:16 +0300

    add Api class

---
+++ src/core/api/api.module.ts
@@ -0,0 +1,31 @@
+import axios, { 
+    AxiosInstance, 
+    AxiosRequestConfig 
+} from 'axios'
+import * as dotenv from 'dotenv' 
+dotenv.config()
+
+const API_URL = process.env.API_URL
+
+class Api {
+    private api: AxiosInstance | null = null
+
+    constructor(){
+        this.api = axios.create({ baseURL: API_URL })
+    }
+
+    request<T>(args: AxiosRequestConfig) {
+        if (!this.api) throw new Error('API is not defined')
+        return this.api.request<T>(args)
+    }
+
+    get<T>(url: string, args: Omit<AxiosRequestConfig, 'method'> = {}) {
+        return this.request<T>({ url, method: 'GET', ...args })
+    }
+
+    post<T>(url: string, args: Omit<AxiosRequestConfig, 'method'> = {}) {
+        return this.request<T>({ url, method: 'POST', ...args })
+      }
+}
+
+export default new Api
